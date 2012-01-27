1dc3b70e80c798722d096dd82e3940970044c935
Jethro Carr <jethro.carr@jethrocarr.com>
2012-01-27 04:03:30 +0000

    * Improved display of logging vs zone sync

--- htdocs/servers/servers.php
+++ htdocs/servers/servers.php
@@ -42,16 +42,18 @@ class page_output
 		$this->obj_table->add_column("standard", "server_name", "");
 		$this->obj_table->add_column("standard", "server_description", "");
 		$this->obj_table->add_column("standard", "server_type", "");
-		$this->obj_table->add_column("standard", "sync_status", "NONE");
+		$this->obj_table->add_column("standard", "sync_status_zones", "NONE");
+		$this->obj_table->add_column("standard", "sync_status_log", "NONE");
 
 		// defaults
-		$this->obj_table->columns		= array("server_primary", "server_record", "server_name", "server_description", "server_type", "sync_status");
+		$this->obj_table->columns		= array("server_primary", "server_record", "server_name", "server_description", "server_type", "sync_status_zones", "sync_status_log");
 		$this->obj_table->columns_order		= array("server_name");
 		$this->obj_table->columns_order_options	= array("server_name");
 
 		$this->obj_table->sql_obj->prepare_sql_settable("name_servers");
 		$this->obj_table->sql_obj->prepare_sql_addfield("id", "");
 		$this->obj_table->sql_obj->prepare_sql_addfield("api_sync_config", "");
+		$this->obj_table->sql_obj->prepare_sql_addfield("api_sync_log", "");
 
 		// load data
 		$this->obj_table->generate_sql();
@@ -67,16 +69,27 @@ class page_output
 			{
 				if ($sync_status_config != $this->obj_table->data[$i]["api_sync_config"])
 				{
-					$this->obj_table->data[$i]["sync_status"]	= "<span class=\"table_highlight_important\">". lang_trans("status_unsynced") ."</span>";
+					$this->obj_table->data[$i]["sync_status_zones"]	= "<span class=\"table_highlight_important\">". lang_trans("status_unsynced") ."</span>";
 				}
 				else
 				{
-					$this->obj_table->data[$i]["sync_status"]	= "<span class=\"table_highlight_open\">". lang_trans("status_synced") ."</span>";
+					$this->obj_table->data[$i]["sync_status_zones"]	= "<span class=\"table_highlight_open\">". lang_trans("status_synced") ."</span>";
+				}
+
+
+				if ((time() - $this->obj_table->data[$i]["api_sync_log"]) > 86400)
+				{
+					$this->obj_table->data[$i]["sync_status_log"]	= "<span class=\"table_highlight_important\">". lang_trans("status_unsynced") ."</span>";
+				}
+				else
+				{
+					$this->obj_table->data[$i]["sync_status_log"]	= "<span class=\"table_highlight_open\">". lang_trans("status_synced") ."</span>";
 				}
 			}
 			else
 			{
-				$this->obj_table->data[$i]["sync_status"]		= "<span class=\"table_highlight_open\">". lang_trans("status_databasesync") ."</span>";
+				$this->obj_table->data[$i]["sync_status_zones"]		= "<span class=\"table_highlight_open\">". lang_trans("status_databasesync") ."</span>";
+				$this->obj_table->data[$i]["sync_status_log"]		= "<span class=\"table_highlight_open\">". lang_trans("status_databasesync") ."</span>";
 			}
 		}
 
