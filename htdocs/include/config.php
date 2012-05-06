64ec2817274ee316823f14fdf0db7117373e5a69
Jethro Carr <jethro.carr@jethrocarr.com>
2012-05-06 09:28:01 +0000

    * Tagged version 1.4.1 with fixes from issue 360

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -24,7 +24,7 @@ $GLOBALS["config"] = array();
 
 // define the application details
 $GLOBALS["config"]["app_name"]			= "NamedManager";
-$GLOBALS["config"]["app_version"]		= "1.4.0";
+$GLOBALS["config"]["app_version"]		= "1.4.1";
 
 // define the schema version required
 $GLOBALS["config"]["schema_version"]		= "20120426";
