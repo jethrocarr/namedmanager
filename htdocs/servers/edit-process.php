d8e6ce6b88dfc91077f4b9f2c9157a28d5816288
Jethro Carr <jethro.carr@jethrocarr.com>
2012-05-06 09:20:56 +0000

    * Added safety check to ensure domain groups always have valid name servers belonging to them. (issue 360)

--- htdocs/servers/edit-process.php
+++ htdocs/servers/edit-process.php
@@ -40,7 +40,7 @@ if (user_permissions_get('namedadmins'))
 	}
 
 	// basic fields
-	$obj_name_server->data["server_name"]			= security_form_input_predefined("any", "server_name", 1, "");
+	$obj_name_server->data["server_name"]			= security_form_input("/^\S*$/", "server_name", 1, "Must be a valid hostname.");
 	$obj_name_server->data["server_description"]		= security_form_input_predefined("any", "server_description", 0, "");
 	$obj_name_server->data["id_group"]			= security_form_input_predefined("int", "id_group", 1, "");
 	$obj_name_server->data["server_primary"]		= security_form_input_predefined("checkbox", "server_primary", 0, "");
@@ -77,6 +77,28 @@ if (user_permissions_get('namedadmins'))
 		log_write("error", "process", "The selected name server group does not exist! Perhaps it has just recently been removed?");
 
 		error_flag_field("id_group");
+	}
+
+
+	// check if the server group has a valid NS record server - if it doesn't, at least one server
+	// added to the domain need to be used as a NS record.
+
+	$obj_sql		= New sql_query;
+	$obj_sql->string	= "SELECT id FROM name_servers WHERE server_record='1' AND id_group='". $obj_name_server->data["id_group"] ."'";
+
+	if ($obj_name_server->id)
+	{
+		$obj_sql->string .= " AND id!='". $obj_name_server->id ."'";
+	}
+
+	$obj_sql->execute();
+
+	if (!$obj_sql->num_rows() && !$obj_name_server->data["server_record"])
+	{
+		log_write("error", "process", "Unable to add this name server to the group - currently there are no other NS record members, make this name server an NS record member when adding it OR add another server to be an NS record for the domain");
+
+		error_flag_field("id_group");
+		error_flag_field("server_record");
 	}
 
 
