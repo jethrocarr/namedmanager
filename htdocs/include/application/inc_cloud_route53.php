a24d0a620a6be340c009aa9a724fc1232149d1b3
Jethro Carr <jethro.carr@jethrocarr.com>
2013-12-16 01:29:22 +1300

    Implemented functionality for syncing records into Route53.
    This works by doing a diff against NamedManager and Route53 and then submiting
    a change batch request to apply the differences. Initial testing shows working
    functionality, but further testing and tuning is expected.
    
    EXPERIMENTAL FEATURE

--- htdocs/include/application/inc_cloud_route53.php
+++ htdocs/include/application/inc_cloud_route53.php
@@ -26,6 +26,7 @@ class cloud_route53
 	public $obj_name_server;
 
 	public $aws_zone_id;
+	public $aws_records;
 
 
 	/*
@@ -91,6 +92,509 @@ class cloud_route53
 		$this->aws_zone_id = sql_get_singlevalue("SELECT id_mapped as value FROM cloud_zone_map WHERE id_name_server='". $this->obj_name_server->id ."' AND id_domain='". $this->obj_domain->id ."' LIMIT 1");
 
 	} // end of select_domain
+
+
+	/*
+		fetch_records_local
+
+		Load all the DNS records for the selected domain into the domain object.
+		---> $this->obj_domain->data["records"]
+
+		Returns
+		0	Failure
+		1	Success
+	*/
+
+	function fetch_records_local()
+	{
+		log_write("debug", "cloud_route53", "Executing fetch_records_local()");
+
+		return $this->obj_domain->load_data_record_all();
+
+	} // end of fetch_records_local()
+
+
+
+	/*
+		fetch_records_remote()
+
+		Fetch all the records from the remote Route53 account for the selected domain.
+		---> $this->aws_records
+
+		Returns
+		0	Failure
+		1	Success
+	*/
+
+	function fetch_records_remote()
+	{
+		log_write("debug", "cloud_route53", "Executing fetch_records_remote()");
+
+		$this->aws_records = array();
+
+		$marker = 0;
+
+		while (true)
+		{
+			// Fetch up to 100 records at a time
+			try {
+				$change = NULL;
+				$change["HostedZoneId"]			= $this->aws_zone_id;
+				$change["MaxItems"]			= '100';
+
+				if ($marker > 0)
+				{
+					$change["Marker"]		= $marker;
+				}
+
+				$query	= $this->obj_route53->listResourceRecordSets($change);
+			
+				if (empty($query["ResourceRecordSets"]))
+				{
+					log_write("error", "cloud_route53", "Invalid resource fetch response returned from Route53");
+					return 0;
+				}
+				else
+				{
+					log_write("debug", "cloud_route53", "Results returned... (block $marker)");
+
+					if ($query["IsTruncated"])
+					{
+					 	// Need to fetch next 100 records
+						$marker = $query["NextMarker"];
+					}
+					else
+					{
+						$marker = 0;
+					}
+				}
+			}
+			catch (Route53Exception $e)
+			{
+				log_write("error", "process", "A failure occured whilst trying to fetch records from AWS/Route53.");
+				log_write("error", "process", "Failure returned: ". $e->getExceptionCode() ."");
+
+				return 0;
+			}
+
+
+			// Walk the array and convert into a format that matches the internal object used by
+			// the domain class.
+			foreach ($query["ResourceRecordSets"] as $route53_record)
+			{
+				// amazon returns some records as a single object with multiple record values
+				// we need to turn them into individual entries, hence the loop
+				
+				for ($i=0; $i < count($route53_record["ResourceRecords"]); $i++)
+				{
+					$tmp		= array();
+
+					// General Record Details
+					$tmp["name"]	= rtrim($route53_record["Name"], '.');
+					$tmp["type"]	= $route53_record["Type"];
+					$tmp["content"]	= rtrim($route53_record["ResourceRecords"][$i]["Value"], '.');
+					$tmp["prio"]	= '0';
+					$tmp["ttl"]	= $route53_record["TTL"];
+
+					// Type-Specific processing
+					switch ($tmp["type"])
+					{
+						case "MX":
+							if (preg_match("/^([0-9]*)\s(\S*)$/", $tmp["content"], $matches))
+							{
+								$tmp["prio"]	= $matches[1];
+								$tmp["content"]	= $matches[2];
+							}
+						break;
+					}
+
+					// AWS records are returned with the full domain path - we need to
+					// remove the domain name to make it suitable for NamedManager usage.
+
+					$tmp["name"] = str_replace(".". $this->obj_domain->data["domain_name"], "", $tmp["name"]);
+
+					$this->aws_records[] = $tmp;
+				}
+			}
+
+
+			unset($query);
+
+			if ($marker == 0)
+			{
+				break;
+			}
+
+		} // end of record fetch loop
+
+
+		return 1;
+
+	} // end of fetch_records_remote()
+
+
+
+
+	/*
+		action_sync_records
+
+		Generate a record changeset, submit to Amazon (dealing with pagination) and track
+		it until actioned. The changeset is generated from the value of the local records
+		and the route53 records that have been loaded.
+
+		This function is a support function and is used by action_create_domain,
+		action_delete_domain and action_update_records.
+
+		Most of the heavy lifting API calls and logic are here.
+
+		Requires
+		- Expects local state in $this->obj_domain->data["records"] (target state to achieve)
+		- Expects remote state in $this->aws_records (old state in Route53).
+
+		Returns
+		0	Uncorrectable Failure
+		1	Success - Changes applied successfully.
+		2	Success - No changes, nothing applied.
+	*/
+
+	function action_sync_records()
+	{
+		log_write("debug", "cloud_route53", "Executing action_sync_records()");
+
+
+		/*
+			Amazon has an annoying fashion of merging records when the name/type are the
+			same, but the contents differ. We need need to apply merging logic here,
+			otherwise the change will fail due to us trying to create multiple records
+			that are the same.
+		
+			This works by us running through the ID lists, generating a hash of the
+			unique record fields (name, type, etc) with all the IDs that match. We can
+			then generate a single record with multiple values accurately that can then
+			be used to compare for diffs.
+
+			It's also worth noting that NamedManager and Bind technically allow different
+		*/
+
+		log_write("debug", "cloud_route53", "Applying merge of records to match Route53 record design");
+
+
+		// Current Local Records
+		$merge_hash_local	= array();
+		$data_records_local	= array();
+
+		for ($i=0; $i < count($this->obj_domain->data["records"]); $i++)
+		{
+			$merge_hash_local[ $this->obj_domain->data["records"][$i]["type"] ."_". $this->obj_domain->data["records"][$i]["name"] ][] = $i;
+		}
+
+		foreach ($merge_hash_local as $ids)
+		{
+			// For every hash entry, all the record details are the same EXCEPT
+			// for the contents. Take the values from the first matched record, then
+			// grab each content value and assume an array record.
+
+			$tmp = array();
+			$id  = $ids[0];
+
+
+			// Exclude any NS records for the domain itself (but still include subdomains)
+			// AWS manages NS records for us, so we don't want to mess with them.
+			if ($this->obj_domain->data["records"][$id]["type"] == "NS" &&
+				$this->obj_domain->data["records"][$id]["name"] = $this->obj_domain->data["domain_name"])
+			{
+				continue;
+			}
+			
+
+			$tmp["Name"]		= $this->obj_domain->data["records"][$id]["name"] .".";
+			$tmp["Type"]		= $this->obj_domain->data["records"][$id]["type"];
+			$tmp["TTL"]		= $this->obj_domain->data["records"][$id]["ttl"];
+			$tmp["ResourceRecords"]	= array();
+
+			foreach ($ids as $id)
+			{
+				$tmp2 = array();
+
+				switch ($tmp["Type"])
+				{
+					case "MX":
+						// MX records are special - need to re-merge priority and content together
+						$tmp2["Value"]	= $this->obj_domain->data["records"][$id]["prio"] ." ". $this->obj_domain->data["records"][$id]["content"] .".";
+					break;
+
+					case "NS":
+					case "CNAME":
+					case "SRV":
+					case "PTR":
+						// These record types need to point to FQDNs, so need the trailing .
+						$tmp2["Value"] = $this->obj_domain->data["records"][$id]["content"] .".";
+					break;
+
+					case "A":
+					case "AAAA":
+					case "TXT":
+					case "SPF":
+					default:
+						// Point to IPs or quoted strings. Don't touch.
+						$tmp2["Value"] = $this->obj_domain->data["records"][$id]["content"];
+					break;
+				}
+				
+				$tmp["ResourceRecords"][] = $tmp2;
+			}
+
+
+			// Adjust the record name to FQDN - AWS won't accept anything else
+			if ($tmp["Name"] != $this->obj_domain->data["domain_name"])
+			{
+				$tmp["Name"] .=  $this->obj_domain->data["domain_name"] .".";
+			}
+			
+			$data_records_local[] = $tmp;
+		}
+
+		unset($merge_hash_local);
+
+
+		// Existing Route53 Records
+		$merge_hash_route53	= array();
+		$data_records_route53	= array();
+
+		for ($i=0; $i < count($this->aws_records); $i++)
+		{
+			$merge_hash_route53[ $this->aws_records[$i]["type"] ."_". $this->aws_records[$i]["name"] ][] = $i;	
+		}
+
+		foreach ($merge_hash_route53 as $ids)
+		{
+			// For every hash entry, all the record details are the same EXCEPT
+			// for the contents. Take the values from the first matched record, then
+			// grab each content value and assume an array record.
+
+			$tmp = array();
+			$id  = $ids[0];
+
+
+			// We want to exclude the SOA record, as we can't include it in our other
+			// comparisons betwee local and Route53.
+			if ($this->aws_records[$id]["type"] == "SOA")
+			{
+				continue;
+			}
+
+			// Exclude any NS records for the domain itself (but still include subdomains)
+			// AWS manages NS records for us, so we don't want to mess with them.
+			if ($this->aws_records[$id]["type"] == "NS" &&
+				$this->aws_records[$id]["name"] = $this->obj_domain->data["domain_name"])
+			{
+				continue;
+			}
+				
+
+			$tmp["Name"]		= $this->aws_records[$id]["name"] .".";
+			$tmp["Type"]		= $this->aws_records[$id]["type"];
+			$tmp["TTL"]		= $this->aws_records[$id]["ttl"];
+			$tmp["ResourceRecords"]	= array();
+
+			foreach ($ids as $id)
+			{
+				$tmp2 = array();
+
+				switch ($tmp["Type"])
+				{
+					case "MX":
+						// MX records are special - need to re-merge priority and content together
+						$tmp2["Value"] = $this->aws_records[$id]["prio"] ." ". $this->obj_domain->data["records"][$id]["content"] .".";
+					break;
+
+					case "NS":
+					case "CNAME":
+					case "SRV":
+					case "PTR":
+						// These record types need to point to FQDNs, so need the trailing .
+						$tmp2["Value"] = $this->aws_records[$id]["content"] .".";
+					break;
+
+					case "A":
+					case "AAAA":
+					case "TXT":
+					case "SPF":
+					default:
+						// Point to IPs or quoted strings. Don't touch.
+						$tmp2["Value"] = $this->aws_records[$id]["content"];
+					break;
+				}
+				
+				$tmp["ResourceRecords"][] = $tmp2;
+			}
+
+			// Adjust the record name to FQDN - AWS won't accept anything else
+			if ($tmp["Name"] != $this->obj_domain->data["domain_name"])
+			{
+				$tmp["Name"] .=  $this->obj_domain->data["domain_name"] .".";
+			}		
+
+			$data_records_route53[] = $tmp;
+		}
+
+		unset($merge_hash_route53);
+
+
+
+		/*
+			We now have our two array structures in a format that will suite Route53. We need
+			to loop through and compare them and generate an array of additions/deletions.
+			(note that changes are both a deletion and addition).
+		*/
+		
+		log_write("debug", "cloud_route53", "Generating Change Batch diff data)");
+
+
+		$ids_local_created  	= array();	// array of Local IDs to create in Route53
+		$ids_route53_deleted	= array();	// array of Route53 IDs to delete from Route53 (has changed)
+		$ids_route53_nochange	= array();	// records with no change.
+
+		for ($il=0; $il < count($data_records_local); $il++)
+		{
+			$match = 0;
+
+			// check if the local record exists in Route53
+			for ($ir=0; $ir < count($data_records_route53); $ir++)
+			{
+				if (($data_records_route53[$ir]["Type"] == $data_records_local[$il]["Type"])
+					&& ($data_records_route53[$ir]["Name"] == $data_records_local[$il]["Name"])
+					&& ($data_records_route53[$ir]["ResourceRecords"] == $data_records_local[$il]["ResourceRecords"]))
+				{
+					$match = 1;
+
+					$diff = array_diff($data_records_route53[$ir], $data_records_local[$il]);
+
+					if (!empty($diff))
+					{
+						// the record exists, but has changed some of it's attributes
+						$ids_route53_deleted[]	= $ir; // delete existing record.
+						$ids_local_created[] 	= $il; // create a new proper record.
+					}
+					else
+					{
+						// record exists and is unchanged - nothing needs to be done for this one.
+						$ids_route53_nochange[] = $ir;
+					}
+				}
+			}
+
+			if (!$match)
+			{
+				// Doesn't exist in Route53 - mark it as a new addition.
+				$ids_local_created[] = $il;
+			}
+		}
+
+
+		// Each unmatched ID is a record that doesn't exist in Route53, we need to
+		// add each of these records to the deletion list.
+		
+		for ($i=0; $i < count($data_records_route53); $i++)
+		{
+			if (!in_array($i, $ids_route53_deleted)	
+				&& !in_array($i, $ids_route53_nochange))
+			{
+				// Record exists in Route53, but isn't in the unchanged list or the
+				// deleted list when compared with local NamedManager. This record must
+				// have been deleted, we now need to purge it.
+
+				$ids_route53_deleted[] = $i;
+			}
+		}
+
+	
+		/*
+			Verify that there has actually been a change!
+		*/
+		if (empty($ids_route53_deleted) && empty($ids_local_created))
+		{
+			log_write("debug", "cloud_route53", "No changes between Route53 and NamedManager");
+			return 2;
+		}
+		else
+		{
+			log_write("debug", "cloud_route53", "There are differences between Route53 and NamedManager. Applying...");
+		}
+
+
+		/*
+			Now we have the lists of IDs that have been added, deleted or unchanged, we can generate our diff.
+		*/
+
+		$data_change_batch = array();
+
+		foreach($ids_route53_deleted as $id)
+		{
+			$tmp = array();
+			$tmp["Action"]			= "DELETE";
+			$tmp["ResourceRecordSet"]	= $data_records_route53[ $id ];
+			$data_change_batch[]		= $tmp;
+		}
+
+		foreach($ids_local_created as $id)
+		{
+			$tmp = array();
+			$tmp["Action"]			= "CREATE";
+			$tmp["ResourceRecordSet"]	= $data_records_local[ $id ];
+			$data_change_batch[]		= $tmp;
+		}
+
+		unset($data_records_local);
+		unset($data_records_route53);
+
+
+		// Finally, append an SOA record that updates the current SOA to use the
+		// domain serial defined in the application.
+
+
+
+
+		// Submit the change to Amazon AWS.
+		log_write("debug", "cloud_route53", "Submitting batch change request to Amazon AWS.");
+
+			try {
+				$change = NULL;
+				$change["HostedZoneId"]			= $this->aws_zone_id;
+				$change["ChangeBatch"]["Changes"]	= $data_change_batch;
+
+				// If debugging, uncommenting the following will dump out the entire change
+				// object before it's sent to AWS.
+				print_r($change);
+//				die("debug");
+
+				$query	= $this->obj_route53->changeResourceRecordSets($change);
+			
+				if (empty($query["ChangeInfo"]))
+				{
+					log_write("error", "cloud_route53", "Invalid change response returned from Route53");
+					return 0;
+				}
+				else
+				{
+					log_write("debug", "cloud_route53", "Batch change submitted successfully.");
+
+				}
+			}
+			catch (Route53Exception $e)
+			{
+				log_write("error", "process", "A failure occured whilst trying to submit a batch change from AWS/Route53.");
+				log_write("error", "process", "Failure returned: ". $e->getExceptionCode() ."");
+
+				return 0;
+			}
+
+		// Verify the change has been applied OK.
+
+
+		return 1;
+
+	} // end of action_sync_records
 
 
 
@@ -177,18 +681,25 @@ class cloud_route53
 		$current_soa = sql_get_singlevalue("SELECT soa_serial as value FROM dns_domains WHERE id='". $this->obj_domain->id ."' LIMIT 1");
 
 
-		// TODO: need this code
-
 		// Fetch all the records for this domain from AWS
+		if (!$this->fetch_records_remote())
+		{
+			return 0;
+		}
 
 		// Fetch all the records for this domain from NamdManager
+		if (!$this->fetch_records_local())
+		{
+			return 0;
+		}
 
 		// Generate a batch job for AWS with the differences to be applied
+		if (!$this->action_sync_records())
+		{
+			return 0;
+		}
 
-			// remember to include the SOA record itself.
-
-
-		// update SOA for cloud zone map table
+		// Update SOA for cloud zone map table
 		$obj_sql 	 = New sql_query;
 		$obj_sql->string = "UPDATE cloud_zone_map SET soa_serial='$current_soa' WHERE id_name_server='". $this->obj_name_server->id ."' AND id_domain='". $this->obj_domain->id ."' LIMIT 1";
 		$obj_sql->execute();
@@ -219,10 +730,24 @@ class cloud_route53
 			Amazon won't delete a domain if there are any records other than the base
 			SOA and NS records assigned to it. We need to submit a delete request for
 			all the records first.
+
+			We do this by setting the local object to empty records, then do a sync
+			record action against Route53. This generates a batch change to delete
+			all the records.
 		*/
 
-		// TODO: need this code
+		$this->obj_domain->data["records"] = array();
 
+		if (!$this->fetch_records_remote())
+		{
+			return 0;
+		}
+
+		if (!$this->action_sync_records())
+		{
+			return 0;
+		}
+	
 
 		/*
 			Delete Domain
